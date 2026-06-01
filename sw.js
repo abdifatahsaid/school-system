0f7eb20a11cfa9e84068731ab59cd6bfef2c89ff
abdifatahsaid <abdifatahsaid908@gmail.com>
2026-06-01 13:23:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-const CACHE_NAME = 'school-system-v1';
-const urlsToCache = ['/', '/static/css/style.css', '/static/js/main.js', '/static/images/logo.png'];
+const CACHE_NAME = 'school-system-v2';
+const urlsToCache = [
+  '/login',
+  '/static/css/style.css',
+  '/static/images/logo.png'
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).catch(err => console.log('Cache error:', err)));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache error:', err))
+  );
 });
+
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request).then(response => {
-        if (response) return response;
-        return fetch(event.request).catch(() => caches.match('/'));
-    }));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => caches.match('/login'));
+    })
+  );
 });
+
 self.addEventListener('activate', event => {
-    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
 });
